baece4381480f1c53f50f53dae4d38a51f2b8fd4
hamed emade <hamadaemadeldine@gmail.com>
2020-05-01 05:50:27 +0200

    Updates

---
+++ precache-manifest.51e3dda658e0a4a063d98ba518ddf223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5343b4ac2fc0b5a7d064a6da973a9bae",
+    "url": "/pics-app/index.html"
+  },
+  {
+    "revision": "4645692fe6964b8580e6",
+    "url": "/pics-app/static/css/main.b1529bc0.chunk.css"
+  },
+  {
+    "revision": "4803a25029b11647689e",
+    "url": "/pics-app/static/js/2.c73d4bb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics-app/static/js/2.c73d4bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4645692fe6964b8580e6",
+    "url": "/pics-app/static/js/main.4555e3d1.chunk.js"
+  },
+  {
+    "revision": "64e8f288d560be55e66f",
+    "url": "/pics-app/static/js/runtime-main.51731ea6.js"
+  }
+]);
